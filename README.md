ef927d4383e0f7603db32e6fc1d05e2a95b9b52b
Sean Grogan <sean.grogan@gmail.com>
2019-10-17 11:13:46 -0400

    Added fizz_buzz_with_kwargs, and added some comments, updated readme.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# fizz_buzz
+# `fizzbuzz`
 A testbed to try different fizz buzz coding practices.
 
 ## Recent results:
@@ -11,3 +11,22 @@ A testbed to try different fizz buzz coding practices.
 | fizz_buzz_v4         | 82.15 |
 | fizz_buzz_one_line   | 60.34 |
 
+## Functions
+
+`fizz_buzz_v1` :  A naive fizz-buzz implementation.
+
+`fizz_buzz_explicit` : A naive fizz-buzz implementation that leverages some of python's coding practices.
+
+`fizz_buzz_generic` :  A fizz-buzz implementation that allows any definition of fizz-buzz words.
+
+`fizz_buzz_one_line` : FizzBuzz Code Golf
+
+`fizz_buzz_with_kwargs` : Playing around with fizz-buzz and python I realized that you can unpack some arguments after arguments. This 
+    might be my most general fizz-buzz function.  It also catches bad arguments like trying to fizz-buzz on a string.
+    :param count: The upper bound of the fizz-buzz, exclusive.  i.e. from [1, count)
+    :param increment: range() step.  Defaults to 1.
+    :param start: What number to start the fizz-buzz from.  Defaults to 1
+    :param fizzy_wds: fizzy words to fizz-buzz.  If left empty, defaults to fizz at 3, buzz at 5. You can pass a 
+    fizz-buzz word as an argument, e.g. (sean=6, tim=4, holly=3), and it will print the argument on the value. If you 
+    want spaces in words, pass any non-defined argument name with a dict with the space. 
+    E.g. dummy={'f i z z': 3,'b u z z':5}
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,34 @@
-p = print
-
-
-def print(l=None, end='\n', doit=False):
-    if doit:
-        print(l, end=end)
-    pass
+def fizz_buzz_with_kwargs(count=30, start=1, increment=1, **fizzy_wds):
+    """ Playing around with fizz-buzz and python I realized that you can unpack some arguments after arguments. This
+    might be my most general fizz-buzz function.  It also catches bad arguments like trying to fizz-buzz on a string.
+    :param count: The upper bound of the fizz-buzz, exclusive.  i.e. from [1, count)
+    :param increment: range() step.  Defaults to 1.
+    :param start: What number to start the fizz-buzz from.  Defaults to 1
+    :param fizzy_wds: fizzy words to fizz-buzz.  If left empty, defaults to fizz at 3, buzz at 5. You can pass a
+    fizz-buzz word as an argument, e.g. (sean=6, tim=4, holly=3), and it will print the argument on the value. If you
+    want spaces in words, pass any non-defined argument name with a dict with the space.
+    E.g. dummy={'f i z z': 3,'b u z z':5}
+    """
+    fizzy_wds = {"fizz": 3, "buzz": 5} if not bool(fizzy_wds) else fizzy_wds
+    for k, v in fizzy_wds.copy().items():
+        if isinstance(v, dict):
+            # Finding dicts in arguments.
+            fizzy_wds.update(v)
+            fizzy_wds.pop(k, None)
+        if not isinstance(v, int) and not isinstance(v, float):
+            # Checking if the arguments are passed as numbers.
+            fizzy_wds.pop(k, None)
+    # Finally printing the fizzy words.
+    for i in range(start, count, increment):
+        line = str().join(word if i % multiple == 0 else "" for word, multiple in fizzy_wds.items())
+        line += str(i) if len(line) == 0 else ""
+        print(line)
 
 
 def fizz_buzz_v1(fizz=3, buzz=5, count=30):
+    """
+    A naive fizz-buzz implementation.
+    """
     i = 1
     while i < count:
         if i % fizz == 0:
@@ -21,6 +42,9 @@ def fizz_buzz_v1(fizz=3, buzz=5, count=30):
 
 
 def fizz_buzz_explicit(fizz=3, buzz=5, count=30):
+    """
+    A naive fizz-buzz implementation that leverages some of python's coding practices.
+    """
     for i in range(1, count):
         line = ""
         line += "fizz" if i % fizz == 0 else ""
@@ -30,6 +54,9 @@ def fizz_buzz_explicit(fizz=3, buzz=5, count=30):
 
 
 def fizz_buzz_generic(fizzy_words={"fizz": 3, "buzz": 5}, count=30):
+    """
+    A fizz-buzz implementation that allows any definition of fizz-buzz words.
+    """
     for i in range(1, count):
         line = str().join(word if i % multiple == 0 else "" for word, multiple in fizzy_words.items())
         line += str(i) if len(line) == 0 else ""
@@ -37,7 +64,13 @@ def fizz_buzz_generic(fizzy_words={"fizz": 3, "buzz": 5}, count=30):
 
 
 def fizz_buzz_v4(fizz=3, buzz=5, count=30):
+    """
+    I have no idea what I was trying to accomplish here...
+    """
+
     def print_num(c, args):
+        """ It was probably something to do with this? :param c: :param args: :return: True or False??
+        """
         return max(1 if c % multiple == 0 else 0 for word, multiple in args.items()) < 1
 
     for i in range(1, count):
@@ -51,27 +84,12 @@ def fizz_buzz_v4(fizz=3, buzz=5, count=30):
         i += 1
 
 
-def fizz_buzz_one_line(fizzy_words={"fizz": 3, "buzz": 5}, num=30):
-    print("\n".join("".join(w if i % m == 0 else "" for w, m in fizzy_words.items()) if max(
-        i % m == 0 for m in fizzy_words.values()) > 0 else str(i) for i in range(1, num)))
+def fizz_buzz_one_line(fzy_wrds={"fizz": 3, "buzz": 5}, num=30):
+    """FizzBuzz Code Golf"""
+    print("\n".join("".join(w if i % m == 0 else "" for w, m in fzy_wrds.items()) if max(
+        i % m == 0 for m in fzy_wrds.values()) > 0 else str(i) for i in range(1, num)))
 
 
 if __name__ == "__main__":
-    times = {}
-    import timeit
-
-    times["fizz_buzz_v1"] = timeit.timeit(fizz_buzz_v1)
-    times["fizz_buzz_explicit"] = timeit.timeit(fizz_buzz_explicit)
-    times["fizz_buzz_generic"] = timeit.timeit(fizz_buzz_generic)
-    times["fizz_buzz_v4"] = timeit.timeit(fizz_buzz_v4)
-    times["fizz_buzz_one_line"] = timeit.timeit(fizz_buzz_one_line)
-
-
-    def line(name, time, times):
-        return f'{name:<{(2 + max(len(t) for t in times))}} : {time:f<.{4}}'
-
-
-    v = max(len(line(name, time, times)) for name, time in times.items())
-    p(f'+={"=":=<{v}}=+')
-    [p(f"| {line(name, time, times)} |") for name, time in times.items()]
-    p(f'+={"=":=<{v}}=+')
+    print('Testing FizzBuzzes')
+    fizz_buzz_with_kwargs()
